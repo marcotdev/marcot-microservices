326389a6ea1f0fff97a842fe8c2fe3fd1e4aac5c
marcotdev <marcotdev@gmail.com>
2023-04-15 12:23:20 -0300

    Created microservice-cambio

---
+++ greeting-config-server/src/test/java/br/com/marcot/greeting/GreetingConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcot.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
