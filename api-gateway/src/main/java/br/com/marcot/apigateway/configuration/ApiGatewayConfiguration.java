0eb3d78005173c7d69680f024216bf5aa8781ac1
marcotdev <marcotdev@gmail.com>
2023-04-23 10:55:10 -0300

    Add LoggingFilter

--- api-gateway/src/main/java/br/com/marcot/apigateway/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/br/com/marcot/apigateway/configuration/ApiGatewayConfiguration.java
@@ -1,11 +1,8 @@
 package br.com.marcot.apigateway.configuration;
 
-import java.util.function.Function;
 
-import org.springframework.cloud.gateway.route.Route;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
